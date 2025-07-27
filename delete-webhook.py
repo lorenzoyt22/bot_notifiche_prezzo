76705c652d8e1a9265f3ef6441f0f55b8c1911a0
lorenzoyt22 <lorenzoyt22@gmail.com>
2025-07-28 01:13:32 +0200

    Update delete-webhook.py

--- delete-webhook.py
+++ delete-webhook.py
@@ -2,10 +2,11 @@ import os
 import requests
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+
 if not BOT_TOKEN:
-    print("❌ BOT_TOKEN mancante nelle variabili d’ambiente")
+    print("❌ BOT_TOKEN mancante")
     exit(1)
 
 url = f"https://api.telegram.org/bot{BOT_TOKEN}/deleteWebhook"
-r = requests.get(url)
-print("✅ Risposta Telegram:", r.text)
+response = requests.get(url)
+print("🧹 Webhook eliminato:", response.text)
